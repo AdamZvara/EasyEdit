cfabc874dd230b7b9d17a5ae738c4bc771bbee69
Adam Zvara <zvaradam@gmail.com>
2026-04-18 13:56:55 +0200

    UnKe hyperparameters

--- easyeditor/models/unke/unke_hparams.py
+++ easyeditor/models/unke/unke_hparams.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import List, Literal
 
+import yaml
+
 from ...util.hparams import HyperParams
 
 @dataclass
@@ -27,4 +29,19 @@ class unkeHyperParams(HyperParams):
     attn_module_tmp: str
     ln_f_module: str
     lm_head_module: str
+
+    @classmethod
+    def from_hparams(cls, hparams_name_or_path: str):
+        if '.yaml' not in hparams_name_or_path:
+            hparams_name_or_path = hparams_name_or_path + '.yaml'
+
+        with open(hparams_name_or_path, "r") as stream:
+            config = yaml.safe_load(stream)
+            config = super().construct_float_from_scientific_notation(config)
+
+        assert (config and config['alg_name'] == 'UnKe') or print(
+            f'unkeHyperParams can not load from {hparams_name_or_path}, '
+            f'alg_name is {config["alg_name"]}'
+        )
+        return cls(**config)
 
